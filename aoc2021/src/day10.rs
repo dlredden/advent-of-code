fc11004655c9468c641b4a4aafd85e9c548f4232
DL-APA <dredden@poolplayers.com>
2022-12-04 08:40:47 -0600

    2021 formatting

--- aoc2021/src/day10.rs
+++ aoc2021/src/day10.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
+use std::fs;
 
 lazy_static! {
     static ref PAIRS: HashMap<&'static str, &'static str> = {
@@ -9,7 +9,7 @@ lazy_static! {
         pairs.insert("[", "]");
         pairs.insert("{", "}");
         pairs.insert("<", ">");
-				pairs.insert(")", "(");
+        pairs.insert(")", "(");
         pairs.insert("]", "[");
         pairs.insert("}", "{");
         pairs.insert(">", "<");
@@ -18,124 +18,114 @@ lazy_static! {
 }
 
 pub fn run() {
-	let data = fs::read_to_string("./data/day10.txt").expect("Error reading file!");
+    let data = fs::read_to_string("./data/day10.txt").expect("Error reading file!");
 
-	let p1_results = part1(&data);
-	println!("Total syntax error score: {}", p1_results);
+    let p1_results = part1(&data);
+    println!("Total syntax error score: {}", p1_results);
 
-	// let p2_results = part2(&data);
-	// println!("total syntax error score part 2: {}", p2_results);
+    // let p2_results = part2(&data);
+    // println!("total syntax error score part 2: {}", p2_results);
 }
 
 fn part1(data: &str) -> usize {
-	let navigation_data: Vec<&str> = parse_data(&data);
-	let mut expected: HashMap<&str, usize> = HashMap::new();
+    let navigation_data: Vec<&str> = parse_data(&data);
+    let mut expected: HashMap<&str, usize> = HashMap::new();
 
-	for line in navigation_data.iter() {
-		let mut opens: Vec<&str> = Vec::new();
-		let chars: Vec<&str> = line
-			.split_terminator("")
-			.skip(1)
-			.collect::<Vec<&str>>();
-		
-		// println!("Line: {:?}", line);
-		for v in chars.iter() {
-			if is_open(*v) {
-				opens.push(*v);
-				continue;
-			}
+    for line in navigation_data.iter() {
+        let mut opens: Vec<&str> = Vec::new();
+        let chars: Vec<&str> = line.split_terminator("").skip(1).collect::<Vec<&str>>();
 
-			let last_open: &str = opens.pop().unwrap_or("");
-			if last_open != *PAIRS.get(*v).unwrap_or(&"") {
-				// println!("Expected {:?}, found {}", *PAIRS.get(*v).unwrap_or(&""), last_open);
-				*expected.entry(*v).or_insert(0) += 1;
-				break;
-			}
-		}
-	}
+        // println!("Line: {:?}", line);
+        for v in chars.iter() {
+            if is_open(*v) {
+                opens.push(*v);
+                continue;
+            }
 
-	calculate_syntax_score(&expected)
+            let last_open: &str = opens.pop().unwrap_or("");
+            if last_open != *PAIRS.get(*v).unwrap_or(&"") {
+                // println!("Expected {:?}, found {}", *PAIRS.get(*v).unwrap_or(&""), last_open);
+                *expected.entry(*v).or_insert(0) += 1;
+                break;
+            }
+        }
+    }
+
+    calculate_syntax_score(&expected)
 }
 
 fn part2(data: &str) -> usize {
-	let navigation_data: Vec<&str> = parse_data(&data);
-	let mut incomplete: Vec<&str> = HashMap::new();
+    let navigation_data: Vec<&str> = parse_data(&data);
+    let mut incomplete: Vec<&str> = Vec::new();
 
-	for line in navigation_data.iter() {
-		let mut opens: Vec<&str> = Vec::new();
-		let chars: Vec<&str> = line
-			.split_terminator("")
-			.skip(1)
-			.collect::<Vec<&str>>();
-		
-		for v in chars.iter() {
-			if is_open(*v) {
-				opens.push(*v);
-				continue;
-			}
+    for line in navigation_data.iter() {
+        let mut opens: Vec<&str> = Vec::new();
+        let chars: Vec<&str> = line.split_terminator("").skip(1).collect::<Vec<&str>>();
 
-			let last_open: &str = opens.pop().unwrap_or("");
-			if last_open != *PAIRS.get(*v).unwrap_or(&"") {
-				break;
-			}
-		}
+        for v in chars.iter() {
+            if is_open(*v) {
+                opens.push(*v);
+                continue;
+            }
 
-		if opens.len() > 0 {
+            let last_open: &str = opens.pop().unwrap_or("");
+            if last_open != *PAIRS.get(*v).unwrap_or(&"") {
+                break;
+            }
+        }
 
-		}
-	}
+        if opens.len() > 0 {}
+    }
 
-	calculate_autocomplete_score(&incomplete)
+    calculate_autocomplete_score(&incomplete)
 }
 
 fn parse_data(data: &str) -> Vec<&str> {
-	let lines: Vec<&str> = data
-		.lines()
-		.collect();
+    let lines: Vec<&str> = data.lines().collect();
 
-	lines
+    lines
 }
 
 fn is_open(value: &str) -> bool {
-	if value == "(" || value == "[" || value == "{" || value == "<" {
-		return true;
-	}
-	false
+    if value == "(" || value == "[" || value == "{" || value == "<" {
+        return true;
+    }
+    false
 }
 
 fn calculate_autocomplete_score(data: &Vec<&str>) -> usize {
-	let mut point_dictionary: HashMap<&str, usize> = HashMap::new();
-	point_dictionary.insert(")", 1);
-	point_dictionary.insert("]", 2);
-	point_dictionary.insert("}", 3);
-	point_dictionary.insert(">", 4);
+    let mut point_dictionary: HashMap<&str, usize> = HashMap::new();
+    point_dictionary.insert(")", 1);
+    point_dictionary.insert("]", 2);
+    point_dictionary.insert("}", 3);
+    point_dictionary.insert(">", 4);
 
-	let mut score: usize = 0;
-	for v in data.iter() {
-		score += point_dictionary[*k] * v;
-	}
-	score
+    let mut score: usize = 0;
+    for v in data.iter() {
+        score += point_dictionary[*k] * v;
+    }
+    score
 }
 
 fn calculate_syntax_score(data: &HashMap<&str, usize>) -> usize {
-	let mut point_dictionary: HashMap<&str, usize> = HashMap::new();
-	point_dictionary.insert(")", 3);
-	point_dictionary.insert("]", 57);
-	point_dictionary.insert("}", 1197);
-	point_dictionary.insert(">", 25137);
+    let mut point_dictionary: HashMap<&str, usize> = HashMap::new();
+    point_dictionary.insert(")", 3);
+    point_dictionary.insert("]", 57);
+    point_dictionary.insert("}", 1197);
+    point_dictionary.insert(">", 25137);
 
-	let mut score: usize = 0;
-	for (k, v) in data.iter() {
-		score += point_dictionary[*k] * v;
-	}
-	score
+    let mut score: usize = 0;
+    for (k, v) in data.iter() {
+        score += point_dictionary[*k] * v;
+    }
+    score
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
+    use super::*;
 
-	const INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
+    const INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
 (((({<>}<{<{<>}{[]{[]{}
@@ -146,13 +136,13 @@ mod test {
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]";
 
-	#[test]
-	fn p1() {
-		assert_eq!(part1(INPUT), 26397);
-	}
+    #[test]
+    fn p1() {
+        assert_eq!(part1(INPUT), 26397);
+    }
 
-	#[test]
-	fn p2() {
-		assert_eq!(part2(INPUT), 0);
-	}
+    #[test]
+    fn p2() {
+        assert_eq!(part2(INPUT), 0);
+    }
 }
