18c20c7816e53b73a4288d10b953efb44c83c745
DL-APA <dredden@poolplayers.com>
2022-12-03 23:36:34 -0600

    D4P1 - Fully contained pairs

--- aoc2022/src/d4/mod.rs
+++ aoc2022/src/d4/mod.rs
@@ -1,12 +1,38 @@
+use itertools::Itertools;
 pub fn run() {
     let data = include_str!("input.txt");
 
-    println!("D4P1 - ???: {}", part1(&data));
+    println!("D4P1 - Fully contained pairs: {}", part1(&data));
     println!("D4P2 - ???: {}", part2(&data));
 }
 
 fn part1(data: &str) -> usize {
-    12
+    let pairs: Vec<&str> = data.lines().collect();
+    let mut fully_contained = 0;
+
+    for pair in pairs {
+        let x = pair.split(",").collect_vec();
+
+        let (x1, y1) = x[0]
+            .split("-")
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect_tuple()
+            .unwrap();
+
+        let (x2, y2) = x[1]
+            .split("-")
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect_tuple()
+            .unwrap();
+
+        if (x1 <= x2 && y1 >= y2) || (x1 >= x2 && y1 <= y2) {
+            fully_contained += 1;
+        }
+
+        // println!("{:?} {:?} {:?} {:?}", x1, y1, x2, y2);
+    }
+
+    fully_contained
 }
 
 fn part2(data: &str) -> usize {
@@ -20,7 +46,7 @@ mod test {
 
     #[test]
     fn p1() {
-        assert_eq!(part1(INPUT), 12);
+        assert_eq!(part1(INPUT), 2);
     }
 
     #[test]
