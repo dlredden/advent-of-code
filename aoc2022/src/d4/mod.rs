e5d0d9caf058daed3d21e8057f1e0e0d0ccc4505
DL-APA <dredden@poolplayers.com>
2022-12-04 00:02:26 -0600

    D4P2 - All overlapping pairs

--- aoc2022/src/d4/mod.rs
+++ aoc2022/src/d4/mod.rs
@@ -3,7 +3,7 @@ pub fn run() {
     let data = include_str!("input.txt");
 
     println!("D4P1 - Fully contained pairs: {}", part1(&data));
-    println!("D4P2 - ???: {}", part2(&data));
+    println!("D4P2 - All overlapping pairs: {}", part2(&data));
 }
 
 fn part1(data: &str) -> usize {
@@ -28,15 +28,40 @@ fn part1(data: &str) -> usize {
         if (x1 <= x2 && y1 >= y2) || (x1 >= x2 && y1 <= y2) {
             fully_contained += 1;
         }
-
-        // println!("{:?} {:?} {:?} {:?}", x1, y1, x2, y2);
     }
 
     fully_contained
 }
 
 fn part2(data: &str) -> usize {
-    12
+    let pairs: Vec<&str> = data.lines().collect();
+    let mut fully_contained = 0;
+
+    for pair in pairs {
+        let x = pair.split(",").collect_vec();
+
+        let (x1, y1) = x[0]
+            .split("-")
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect_tuple()
+            .unwrap();
+
+        let (x2, y2) = x[1]
+            .split("-")
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect_tuple()
+            .unwrap();
+
+        if (x1 <= x2 && y1 >= y2)
+            || (x1 >= x2 && y1 <= y2)
+            || (x1 <= x2 && y1 <= y2 && y1 >= x2)
+            || (x1 >= x2 && y1 >= y2 && y2 >= x1)
+        {
+            fully_contained += 1;
+        }
+    }
+
+    fully_contained
 }
 
 #[cfg(test)]
@@ -51,6 +76,6 @@ mod test {
 
     #[test]
     fn p2() {
-        assert_eq!(part2(INPUT), 12);
+        assert_eq!(part2(INPUT), 4);
     }
 }
