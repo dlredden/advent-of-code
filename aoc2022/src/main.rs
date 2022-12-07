4a3e5c261c0e70aa5da2a80bbd0a43cc08342297
DL-APA <dredden@poolplayers.com>
2022-12-06 21:03:02 -0600

    Use threads to run days

--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -1,14 +1,29 @@
+use std::thread;
+
 mod d1;
 mod d2;
 mod d3;
 mod d4;
 mod d5;
 mod d6;
+
 fn main() {
-    d1::run();
-    d2::run();
-    d3::run();
-    d4::run();
-    d5::run();
-    d6::run();
+    let mut handles = vec![];
+
+    for i in 1..6 {
+        let handle = thread::spawn(move || match i {
+            1 => d1::run(),
+            2 => d2::run(),
+            3 => d3::run(),
+            4 => d4::run(),
+            5 => d5::run(),
+            6 => d6::run(),
+            _ => println!("No such day"),
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
