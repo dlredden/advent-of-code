ca58ad5d332df34e07d86a5e6a2adb9979a2eef1
DL-APA <dredden@poolplayers.com>
2022-12-06 22:31:15 -0600

    Main comments

--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     let mut handles: HashMap<i32, JoinHandle<(String, String)>> = HashMap::new();
     let mut results: HashMap<i32, (String, String)> = HashMap::new();
 
+    // Spawn threads for each day
     for i in 1..7 {
         let handle = thread::spawn(move || match i {
             1 => d1::run(),
@@ -27,13 +28,17 @@ fn main() {
         handles.insert(i, handle);
     }
 
+    // Join threads and store results
     for (day, handle) in handles {
         let result: (String, String) = handle.join().unwrap();
         results.insert(day, result);
     }
 
+    // Sort results by day
     let mut pairs: Vec<(&i32, &(String, String))> = results.iter().collect();
     pairs.sort_by(|a, b| a.0.cmp(b.0));
+
+    // Print results
     for (day, (p1, p2)) in pairs {
         println!("Day {}.1: {}\nDay {}.2: {}", day, p1, day, p2);
     }
