f78ee2d0c36e174197f1431da41b38e890d7db9a
DL-APA <dredden@poolplayers.com>
2022-12-06 20:24:50 -0600

    Day 6

---
+++ aoc2022/src/d6/mod.rs
@@ -0,0 +1,78 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
+pub fn run() {
+    const DATA: &str = include_str!("input.txt");
+
+    println!("D6P1 - First start of packet marker: {}", part1(&DATA));
+    println!("D6P2 - First start of packet marker: {}", part2(&DATA));
+}
+
+fn part1(data: &str) -> u32 {
+    let mut count = 0;
+    let mut signal: VecDeque<char> = VecDeque::new();
+
+    let chars = data.lines().next().unwrap().chars();
+    for c in chars {
+        count += 1;
+
+        if signal.len() < 4 {
+            signal.push_back(c);
+        } else {
+            signal.pop_front();
+            signal.push_back(c);
+        }
+
+        if signal.iter().unique().count() == 4 {
+            return count;
+        }
+    }
+
+    count
+}
+
+fn part2(data: &str) -> u32 {
+    let mut count = 0;
+    let mut signal: VecDeque<char> = VecDeque::new();
+
+    let chars = data.lines().next().unwrap().chars();
+    for c in chars {
+        count += 1;
+        let length = signal.len();
+        if length < 14 {
+            signal.push_back(c);
+        } else {
+            signal.pop_front();
+            signal.push_back(c);
+        }
+        if signal.iter().unique().count() == 14 {
+            return count;
+        }
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn p1() {
+        assert_eq!(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn p2() {
+        assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
+}
--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -3,11 +3,12 @@ mod d2;
 mod d3;
 mod d4;
 mod d5;
-
+mod d6;
 fn main() {
     d1::run();
     d2::run();
     d3::run();
     d4::run();
     d5::run();
+    d6::run();
 }
