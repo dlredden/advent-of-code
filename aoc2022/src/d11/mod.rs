e2e6a52bfe2d9a44a9e46acce9bdfa9760aab32b
DL-APA <dredden@poolplayers.com>
2022-12-13 23:17:26 -0600

    Day 11.2

--- aoc2022/src/d11/mod.rs
+++ aoc2022/src/d11/mod.rs
@@ -6,21 +6,21 @@ pub fn run() -> (String, String) {
 }
 
 struct Monkey {
-    inventory: VecDeque<i32>,
-    worry_factor: Option<i32>,
-    worry: fn(&mut i32, Option<i32>),
-    test_factor: i32,
-    test: fn(i32, i32) -> bool,
+    inventory: VecDeque<i64>,
+    worry_factor: Option<i64>,
+    worry: fn(&mut i64, Option<i64>),
+    test_factor: i64,
+    test: fn(i64, i64) -> bool,
     throw_to: HashMap<bool, usize>,
-    inspection_count: i32,
+    inspection_count: i64,
 }
 
 impl Monkey {
     fn new(
-        inventory: VecDeque<i32>,
-        worry_factor: Option<i32>,
-        worry: fn(&mut i32, Option<i32>),
-        test_factor: i32,
+        inventory: VecDeque<i64>,
+        worry_factor: Option<i64>,
+        worry: fn(&mut i64, Option<i64>),
+        test_factor: i64,
         throw_to: HashMap<bool, usize>,
     ) -> Monkey {
         Monkey {
@@ -38,23 +38,30 @@ impl Monkey {
         self.inventory.len()
     }
 
-    pub fn inspect(&mut self, x: &mut i32) -> bool {
+    pub fn inspect_1(&mut self, x: &mut i64) -> bool {
         self.inspection_count += 1;
         (self.worry)(x, self.worry_factor);
         *x /= 3; // relief
         (self.test)(*x, self.test_factor)
     }
 
-    pub fn add_to_inventory(&mut self, x: i32) {
+    pub fn inspect_2(&mut self, x: &mut i64, y: i64) -> bool {
+        self.inspection_count += 1;
+        (self.worry)(x, self.worry_factor);
+        *x %= y;
+        (self.test)(*x, self.test_factor)
+    }
+
+    pub fn add_to_inventory(&mut self, x: i64) {
         self.inventory.push_back(x);
     }
 
-    pub fn get_from_inventory(&mut self) -> i32 {
+    pub fn get_from_inventory(&mut self) -> i64 {
         self.inventory.pop_front().unwrap()
     }
 }
 
-fn worry_multiply(x: &mut i32, multiplier: Option<i32>) {
+fn worry_multiply(x: &mut i64, multiplier: Option<i64>) {
     if let Some(value) = multiplier {
         *x *= value;
     } else {
@@ -62,11 +69,11 @@ fn worry_multiply(x: &mut i32, multiplier: Option<i32>) {
     }
 }
 
-fn worry_add(x: &mut i32, adder: Option<i32>) {
+fn worry_add(x: &mut i64, adder: Option<i64>) {
     *x += adder.unwrap()
 }
 
-fn test_worry(x: i32, divisor: i32) -> bool {
+fn test_worry(x: i64, divisor: i64) -> bool {
     x % divisor == 0
 }
 
@@ -80,8 +87,8 @@ fn parse_input(lines: &[&str]) -> Vec<Monkey> {
             .split(',')
             .collect::<Vec<&str>>()
             .iter()
-            .map(|x| x.trim().parse::<i32>().unwrap())
-            .collect::<VecDeque<i32>>();
+            .map(|x| x.trim().parse::<i64>().unwrap())
+            .collect::<VecDeque<i64>>();
 
         let op = line[2].split_whitespace().collect::<Vec<&str>>();
         let worry = match op[4] {
@@ -89,14 +96,14 @@ fn parse_input(lines: &[&str]) -> Vec<Monkey> {
             "+" => worry_add,
             _ => panic!("Invalid worry"),
         };
-        let worry_factor: Option<i32> = if op[5] == "old" {
+        let worry_factor: Option<i64> = if op[5] == "old" {
             None
         } else {
-            Some(op[5].parse::<i32>().unwrap())
+            Some(op[5].parse::<i64>().unwrap())
         };
         let test_factor = line[3].split_whitespace().collect::<Vec<&str>>()[3]
             .trim()
-            .parse::<i32>()
+            .parse::<i64>()
             .unwrap();
         let mut throw_to: HashMap<bool, usize> = HashMap::new();
         throw_to.insert(
@@ -124,18 +131,17 @@ fn parse_input(lines: &[&str]) -> Vec<Monkey> {
     monkeys
 }
 
-fn part1(data: &str) -> i32 {
+fn part1(data: &str) -> i64 {
     let lines: Vec<&str> = data.lines().collect();
     let mut monkeys = parse_input(&lines);
     let num_monkeys = monkeys.len();
 
     for _i in 0..20 {
         for m in 0..num_monkeys {
-            println!("i: {}, m: {}", _i, m);
             while monkeys[m].inventory_len() > 0 {
                 let mut x = monkeys[m].get_from_inventory();
 
-                let result = monkeys[m].inspect(&mut x);
+                let result = monkeys[m].inspect_1(&mut x);
                 let throw_to = monkeys[m].throw_to[&result];
                 monkeys[throw_to].add_to_inventory(x);
             }
@@ -147,10 +153,31 @@ fn part1(data: &str) -> i32 {
     monkeys[0].inspection_count * monkeys[1].inspection_count
 }
 
-fn part2(data: &str) -> i32 {
+fn part2(data: &str) -> i64 {
     let lines: Vec<&str> = data.lines().collect();
-    let _monkeys = parse_input(&lines);
-    0
+    let mut monkeys = parse_input(&lines);
+    let num_monkeys = monkeys.len();
+    let mut modifier: i64 = 1;
+
+    for i in 0..num_monkeys {
+        modifier *= monkeys[i].test_factor;
+    }
+
+    for _i in 0..10000 {
+        for m in 0..num_monkeys {
+            while monkeys[m].inventory_len() > 0 {
+                let mut x = monkeys[m].get_from_inventory();
+
+                let result = monkeys[m].inspect_2(&mut x, modifier);
+                let throw_to = monkeys[m].throw_to[&result];
+                monkeys[throw_to].add_to_inventory(x);
+            }
+        }
+    }
+
+    monkeys.sort_by(|a, b| b.inspection_count.cmp(&a.inspection_count));
+
+    monkeys[0].inspection_count * monkeys[1].inspection_count
 }
 
 #[cfg(test)]
@@ -165,6 +192,6 @@ mod test {
 
     #[test]
     fn p2() {
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), 2713310158);
     }
 }
