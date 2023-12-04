11196357218404277a4ababaf629f954b6fabe4a
DL-WL <dlredden96@gmail.com>
2023-12-04 07:58:00 -0600

    2023 d4 p1

--- aoc2023/src/d4/mod.rs
+++ aoc2023/src/d4/mod.rs
@@ -4,37 +4,47 @@ pub fn run() -> (String, String) {
 }
 
 fn part1(data: &str) -> i32 {
-    let lines: Vec<&str> = data.lines().collect();
+    let cards: Vec<&str> = data.lines().collect();
+    let mut points = 0;
 
-    for line in lines {
-        println!("{}", line);
+    for card in cards {
+        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        let split_card = card.split(" | ").collect::<Vec<&str>>();
+        let winning_numbers = split_card[0].split(':').collect::<Vec<&str>>()[1]
+            .split_whitespace()
+            .collect::<Vec<&str>>();
+        let my_numbers = split_card[1].split_whitespace().collect::<Vec<&str>>();
+        let matches = winning_numbers
+            .iter()
+            .filter(|&n| my_numbers.contains(n))
+            .count();
+        match matches {
+            0 => continue,
+            1 => points += 1,
+            m if m > 1 => points += 1 << (m - 1),
+            _ => (),
+        }
     }
 
-		0
+    points
 }
 
-fn part2(data: &str) -> i32 {
-    let lines: Vec<&str> = data.lines().collect();
-
-    for line in lines {
-        println!("{}", line);
-    }
-
-		0
+fn part2(_data: &str) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    const INPUT1: &str = include_str!("input.test.txt");
+    const INPUT: &str = include_str!("input.test.txt");
 
     #[test]
     fn p1() {
-        assert_eq!(part1(INPUT1), 0);
+        assert_eq!(part1(INPUT), 13);
     }
 
     #[test]
     fn p2() {
-        assert_eq!(part2(INPUT2), 0);
+        assert_eq!(part2(INPUT), 0);
     }
 }
